a2d2ac755d4468ca576996eaf0658aaadf884a8d
Motekema <motekema@gmai.com>
2023-09-04 03:27:14 -0700

    My commit

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,5 +3,6 @@ word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
-formatted_output = f"First 3 letters: {word_first_3}\nLast 2 letters: {word_last_2}\nMiddle word: {middle_word}"
-print(formatted_output)
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
