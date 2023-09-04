38a244e7ccc48e5492280d85fe59ff3cfc22dd4c
Motekema <motekema@gmai.com>
2023-09-04 03:34:45 -0700

    My commit

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-substring1 = str[39:67]
-substring2 = str[107:112]
-substring3 = str[:6]
-str = substring1 + substring2 + substring3
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
