ddf99a4c550dfdf42d889e7e7516ad8263264353
Motekema <motekema@gmai.com>
2023-09-05 15:43:43 -0700

    My

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-for h in range(ord('z'), ord('a') - 1, -1):
-    letter = chr(h)
-    if letter.islower():
-        print("{}".format(letter), end="")
-        for h in range(ord('Z'), ord('A') - 1, -1):
-            print("{}".format(chr(h)), end="")
-            print()
+for c in range(ord('z'), ord('a') - 1, -1):
+    print(
+        "{}".format(
+            chr(c) if (ord('z') - c) % 2 == 0 else chr(c).upper()
+        ),
+        end=""
+    )
