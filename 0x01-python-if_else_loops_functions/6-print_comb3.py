c3c10cabfc509f9ff9005c1ed7eaa6c8d7df768b
Motekema <motekema@gmai.com>
2023-09-05 08:05:08 -0700

    My commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
-for tens in range(10):
-for ones in range(tens + 1, 10):
-if tens == 8 and ones == 9:
-print("{:d}{:d}".format(tens, ones))
-else:
-print("{:d}{:d}, ".format(tens, ones), end="")
+for t in range(0, 8):
+for z in range(t + 1, 10):
+print("{:d}{:d}".format(t, z), end=', ')
+print("{:d}{:d}".format(t + 1, z))
