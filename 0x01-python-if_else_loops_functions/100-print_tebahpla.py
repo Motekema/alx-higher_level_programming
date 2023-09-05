8e0f6176f5b40580d6ddcf1b7c6ca857060886db
Motekema <motekema@gmai.com>
2023-09-05 15:29:23 -0700

    My commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# 100-print_tebahpla.py
 
 h = 0
 for h in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - h)), end="")
     h = 32 if h == 0 else 0
-
--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,6 @@
 # 101-remove_char_at.py
 
 def remove_char_at(string, n):
-"""Remove the character at position n from the string."""
-if n < 0 or n >= len(string):
-return string
-return string[:n] + string[n+1:]
+    if n < 0 or n >= len(string):
+        return string
+    return string[:n] + string[n+1:]
