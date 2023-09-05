b0ce0cfb60870d39b831d0be4efde3a51c8fd7a4
Motekema <motekema@gmai.com>
2023-09-05 04:29:44 -0700

    My commit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+digit = abs(number) % 10
+
+if number < 0:
+    digit = -digit
+
+comparison_result = "greater than 5" if digit > 5 else "0" if digit == 0 else "less than 6 and not 0"
+
+print("Last digit of {} is {} and is {}".format(number, digit, comparison_result))
