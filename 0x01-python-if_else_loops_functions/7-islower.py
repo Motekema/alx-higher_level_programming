99e9edd0be5ca3cf6dc70fc8703bea38eb753540
Motekema <motekema@gmai.com>
2023-09-05 08:33:51 -0700

    My commit

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 def islower(c):
-    
-    return 97 <= ord(c) <= 122
+
+return 97 <= ord(c) <= 122
 
 def main():
-    print("a is {}".format("lower" if islower("a") else "upper"))
-    print("H is {}".format("lower" if islower("H") else "upper"))
-    print("A is {}".format("lower" if islower("A") else "upper"))
-    print("3 is {}".format("lower" if islower("3") else "upper"))
-    print("g is {}".format("lower" if islower("g") else "upper"))
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
 
 if __name__ == "__main__":
-    main()
+main()
