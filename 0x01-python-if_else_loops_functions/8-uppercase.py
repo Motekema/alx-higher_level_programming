ee29e8ccfaa83ad8f26310dc9e05ccf21b12cb41
Motekema <motekema@gmai.com>
2023-09-05 15:11:33 -0700

    My commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# 8-uppercase.py
+
 def uppercase(str):
     for j in str:
         if ord(j) >= 97 and ord(j) <= 122:
             j = chr(ord(j) - 32)
-        print("{}".format(j), end="")
-        print("")
+            print("{}".format(j), end="")
+            print("")
