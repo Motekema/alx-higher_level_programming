f299d25daab9cce4fb50edded9901797a6448fa7
Motekema <motekema@gmai.com>
2023-09-05 06:14:16 -0700

    My commit

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,9 +3,8 @@
 
 
 def islower(o):
-    """Check for lowercase."""
-    if ord(o) >= 97 and ord(o) <= 122:
-        return True
-    else:
-        return False
-
+"""Check for lowercase."""
+if ord(o) >= 97 and ord(o) <= 122:
+return True
+else:
+return False
