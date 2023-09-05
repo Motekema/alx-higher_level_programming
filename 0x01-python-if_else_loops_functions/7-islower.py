4834a1853081bbc001324f37757d6674b46debc5
Motekema <motekema@gmai.com>
2023-09-05 06:12:01 -0700

    My commit

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-islower = __import__('7-islower').islower
+# 7-islower.py
 
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+
+def islower(o):
+    """Check for lowercase."""
+    if ord(o) >= 97 and ord(o) <= 122:
+        return True
+    else:
+        return False
+
