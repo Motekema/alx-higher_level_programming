e17bb46fcc2a89596c4296882443c7c470ee762e
Motekema <motekema@gmai.com>
2023-09-05 14:58:49 -0700

    My commit

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 
-def islower(c):
-
-return 97 <= ord(c) <= 122
-
-def main():
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
-
-if __name__ == "__main__":
-main()
+def islower(k):
+    
+    if ord('a') <= ord(k) <= ord('z'):
+        return True
+    else:
+        return False
