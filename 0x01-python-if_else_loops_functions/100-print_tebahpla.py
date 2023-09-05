79ea85b354b3cfc8d067e9efe1af037ace216079
Motekema <motekema@gmai.com>
2023-09-05 15:35:38 -0700

    My commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-h = 0
 for h in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(h - h)), end="")
-    h = 32 if h == 0 else 0
+    letters = chr(h)
+    uppercase_letter = letters.upper()
+    print("{}".format(uppercase_letter), end="")
