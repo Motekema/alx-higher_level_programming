3c29b5f881eec24572620a37e15e4bf7ce841a77
Motekema <motekema@gmai.com>
2023-09-05 15:04:52 -0700

    My commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 # 8-uppercase.py
-
-
 def uppercase(str):
-"""Print a string with uppercase."""
-for o in str:
-if ord(o) >= 97 and ord(o) <= 122:
-o = chr(ord(o) - 32)
-print("{}".format(o), end="")
-print("")
-
-
+    for j in str:
+        if ord(j) >= 97 and ord(j) <= 122:
+            j = chr(ord(j) - 32)
+        print("{}".format(j), end="")
+        print("")
