b2c82367950e5042b5d7df382c722b53bcb207e9
Motekema <motekema@gmai.com>
2023-09-05 15:22:52 -0700

    My commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,13 +2,12 @@
 
 
 def fizzbuzz():
-for number in range(1, 101):
-if number % 3 == 0 and number % 5 == 0:
-print("FizzBuzz ", end="")
-elif number % 3 == 0:
-print("Fizz ", end="")
-elif number % 5 == 0:
-print("Buzz ", end="")
-else:
-print("{} ".format(number), end="")
-
+    for numbers in range(1, 101):
+        if numbers % 3 == 0 and numbers % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif numbers % 3 == 0:
+            print("Fizz ", end="")
+        elif numbers % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(numbers), end="")
