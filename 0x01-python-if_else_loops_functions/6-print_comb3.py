b90d3b55ff0fb3cb706934adc8e9f26683d62b3f
Motekema <motekema@gmai.com>
2023-09-05 14:55:27 -0700

    My commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-for t in range(0, 9):
-    for z in range(t + 1, 10):
-        if z < 9:
-            print("{:02d}, ".format(t), end='')
+
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2), end="\n")
         else:
-            print("{:02d}".format(t), end='')
-        print("{:02d}".format(z), end=', ')
-        print("{:02d}".format(89))
+            print("{}{}".format(digit1, digit2), end=", ")
