3c9e15a9a5634667505b0a20701e9e8de6bd73bf
Motekema <motekema@gmai.com>
2023-09-05 14:19:11 -0700

    My commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,5 +3,4 @@ output_string = ""
 for letters in range(97, 123):
     if chr(letters) != 'q' and chr(letters) != 'e':
         output_string += chr(letters)
-
-print(output_string)
+        print(output_string)
