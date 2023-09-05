7cad2d4fb34c8f9db63bb76a708f539ea2bd3d77
Motekema <motekema@gmai.com>
2023-09-05 04:48:21 -0700

    My commit

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # 2-print_alphabet.py
 
-"""Print the alphabet in lowercase, not followed by a new line."""
-for letter in range(97, 123):
+for letter in range(ord('a'), ord('z') + 1):
 print("{}".format(chr(letter)), end="")
