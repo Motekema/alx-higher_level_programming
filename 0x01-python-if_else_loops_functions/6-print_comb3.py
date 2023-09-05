38f81ed4988a282b5217d05a8a70caea993187d9
Motekema <motekema@gmai.com>
2023-09-05 14:52:19 -0700

    My commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-for t in range(0, 8):
+for t in range(0, 9):
     for z in range(t + 1, 10):
-        print("{:d}{:d}".format(t, z), end=', ')
-        print("{:d}{:d}".format(t + 1, z))
+        if z < 9:
+            print("{:02d}, ".format(t), end='')
+        else:
+            print("{:02d}".format(t), end='')
+        print("{:02d}".format(z), end=', ')
+        print("{:02d}".format(89))
