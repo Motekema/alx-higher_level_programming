307171a4fe93a380659d93ca034e003d99d9d340
Motekema <motekema@gmai.com>
2023-09-05 14:33:26 -0700

    My commit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for number in range(100):
-if number < 99:
-print("{:02d}, ".format(number), end="")
-else:
-print("{:02d}".format(number))
+for numbers in range(100):
+    if numbers < 99:
+        print("{:02d}, ".format(numbers), end="")
+    else:
+        print("{:02d}".format(numbers))
