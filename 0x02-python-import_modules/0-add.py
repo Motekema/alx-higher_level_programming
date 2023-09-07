8e4fc0d22de3a78f0f09d090e37a5cbabb612a98
Motekema <motekema@gmai.com>
2023-09-07 06:36:12 -0700

    my commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-def add(a, b):
-    return a + b
 if __name__ == "__main__":
-    a = 1
+    from add_0 import add
     b = 2
+    a = 1
     print("{} + {} = {}".format(a, b, add(a, b)))
