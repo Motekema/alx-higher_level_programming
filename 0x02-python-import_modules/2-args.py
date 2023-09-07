74c2155c2ba3b0b69cf6b90ea76afbec169d53c0
Motekema <motekema@gmai.com>
2023-09-07 07:34:22 -0700

    My commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     import sys
-    coun_num = len(sys.argv) - 1
+    count_num = len(sys.argv) - 1
     if count_num == 0:
         print("0 arguments.")
     elif count_num == 1:
