83287c9f3a659568de2dba89a4e4bfe1fc2aeb17
Motekema <motekema@gmai.com>
2023-09-07 08:07:30 -0700

    My commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,5 +5,4 @@ if __name__ == "__main__":
     total_sum = 0
     for i in range(len(sys.argv) - 1):
         total_sum += int(sys.argv[i + 1])
-    print("{}".format(total_sum))
-
+        print("{}".format(total_sum))
