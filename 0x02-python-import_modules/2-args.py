dc48d402d7f9f2e7f89ed1e0bfbd547ed609e4af
Motekema <motekema@gmai.com>
2023-09-07 07:11:18 -0700

    My commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+import sys
 
-if __name__ == "__main__":
-    import sys
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 2]))
+num_args = len(sys.argv) - 1
+if num_args == 0:
+    print("0 arguments.")
+elif num_args == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(num_args))
+for i in range(num_args):
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
