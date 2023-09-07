59e05b1c3ab013d4f7feb11ebe0b03f4015eec33
Motekema <motekema@gmai.com>
2023-09-07 07:25:10 -0700

    My commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,12 @@
 
 if __name__ == "__main__":
 import sys
-
-num_args = len(sys.argv) - 1
-
-if num_args == 0:
+count = len(sys.argv) - 1
+if count == 0:
     print("0 arguments.")
-elif num_args == 1:
+elif count == 1:
     print("1 argument:")
 else:
-    print("{} arguments:".format(num_args))
-for i in range(num_args):
+    print("{} arguments:".format(count))
+for i in range(count):
     print("{}: {}".format(i + 1, sys.argv[i + 1]))
