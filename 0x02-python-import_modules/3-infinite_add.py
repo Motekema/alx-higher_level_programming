9db2213e786615f4ea71d896ed38b671f3aceb8e
Motekema <motekema@gmai.com>
2023-09-07 07:47:25 -0700

    My commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     import sys
     total_sum = 0
-    for arg in range(len(sys.argv) - 1):
-        total_sum += int(sys.argv[agr + 1])
+    for i in range(len(sys.argv) - 1):
+        total_sum += int(sys.argv[i + 1])
     print("{}".format(total_sum))
+
