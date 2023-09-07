d0478752daab3e76d9742d5e0798eb661405e42f
Motekema <motekema@gmai.com>
2023-09-07 06:43:27 -0700

    My commit

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 
-# Define the values for a and b
-a = 10
-b = 5
-
-# Call and print the results of each function using the specified format
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    b = 5
+    a = 10
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
