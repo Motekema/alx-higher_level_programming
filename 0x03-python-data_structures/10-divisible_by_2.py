4818c1400209159150bf170280b41c2fe05dae6c
Motekema <motekema@gmai.com>
2023-09-11 03:46:11 -0700

    My commit

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+
 def divisible_by_2(my_list=[]):
-    result = []
-    for num in my_list:
-        result.append(num % 2 == 0)
-        return (result)
+    multiples = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            multiples.append(True)
+        else:
+            multiples.append(False)
+
+    return (multiples)
