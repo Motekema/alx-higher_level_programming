10c8b8eb348abd876a78fd1843914f7ec6adec4c
Motekema <motekema@gmai.com>
2023-09-11 02:30:01 -0700

    My commit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,8 +3,6 @@
 def print_list_integer(my_list=[]):
     for integer in my_list:
         print("{:d}".format(integer))
-
-# Example usage:
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    print_list_integer(my_list)
+        if __name__ == "__main__":
+            my_list = [1, 2, 3, 4, 5]
+            print_list_integer(my_list)
