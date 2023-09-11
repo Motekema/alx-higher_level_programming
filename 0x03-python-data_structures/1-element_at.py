5eccf19e018c489971cb89671eede01b4b1a5cfd
Motekema <motekema@gmai.com>
2023-09-11 02:36:08 -0700

    My commit

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return (None)
+    return (my_list[idx])
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    result = element_at(my_list, idx)
+    print("Element at index {:d} is {}".format(idx, result))
