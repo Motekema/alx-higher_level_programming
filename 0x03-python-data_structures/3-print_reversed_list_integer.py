10eeacb3cb0c121d6c11f3a5ccde2fa55ca5ca8b
Motekema <motekema@gmai.com>
2023-09-11 03:04:29 -0700

    My commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     for num in reversed(my_list):
         print(num)
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    print_reversed_list_integer(my_list)
+        if __name__ == "__main__":
+            my_list = [1, 2, 3, 4, 5]
+            print_reversed_list_integer(my_list)
