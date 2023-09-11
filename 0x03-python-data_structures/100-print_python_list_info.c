df31fdd742e3f3b478386b69ed0bf6292c598af6
Motekema <motekema@gmai.com>
2023-09-11 03:56:32 -0700

    My commit

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - Prints basic info about a Python list
+ * @p: Pointer to a Python object (list)
+ */
+void print_python_list_info(PyObject *p)
+{
+    Py_ssize_t size, i;
+    PyObject *item;
+
+    size = PyList_Size(p);
+    printf("[*] Size of the Python List = %ld\n", size);
+
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++)
+    {
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
+
