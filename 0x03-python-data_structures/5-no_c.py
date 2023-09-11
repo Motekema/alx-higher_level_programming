5fdcf40264dc5e5a0b884cde19c4030682b38634
Motekema <motekema@gmai.com>
2023-09-11 03:18:49 -0700

    My commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    """Remove all characters 'c' and 'C' from a string."""
     copy = [x for x in my_string if x != 'c' and x != 'C']
-    return "".join(copy)
-
-if __name__ == "__main__":
-    print(no_c("Best School"))
-    print(no_c("Chicago"))
-    print(no_c("C is fun!"))
+    return ("".join(copy))
