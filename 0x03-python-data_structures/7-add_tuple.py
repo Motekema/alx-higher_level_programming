a3ab772ad0103b99e539ee59646518a3a4473b5d
Motekema <motekema@gmai.com>
2023-09-11 03:28:18 -0700

    My commit

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
-def add_tuple(tuple_a=(0, 0), tuple_b=(0, 0)):
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+def add_tuple(tuple_a=(), tuple_b=()):
+    list_a = list(tuple_a) + [0, 0]
+    list_b = list(tuple_b) + [0, 0]
+    result = [list_a[0] + list_b[0], list_a[1] + list_b[1]]
+    return tuple(result)
