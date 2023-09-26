69481a2496fd7d0fd75bd2b8212ca41107d289d5
Motekema <motekema@gmail.com>
2023-09-26 07:13:52 +0000

    My commit

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,4 +1,6 @@
-st_division(my_list_1, my_list_2, list_length):
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
