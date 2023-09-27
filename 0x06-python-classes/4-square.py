c0be4eea2e8efe659763e354d54395077734994c
Motekema <motekema@gmail.com>
2023-09-27 14:41:39 +0000

    My commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
+"""Module documentation - Represents a square."""
 
 class Square:
+    """Class documentation - Represents a square."""
+
     def __init__(self, size=0):
-        """ Initialize the property size with the provided size."""
+        """Initialize the property size with the provided size."""
         self.size = size
 
     @property
     def size(self):
-        """ Getter method for retrieving the size attribute."""
+        """Getter method for retrieving the size attribute."""
         return self._size
 
     @size.setter
     def size(self, value):
-        """ Setter method for setting the size attribute and performing type and value validation."""
+        """Setter method for setting the size attribute and performing type and value validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -21,5 +24,5 @@ class Square:
             self._size = value
 
     def area(self):
-        """ Calculate and return the area of the square."""
+        """Calculate and return the area of the square."""
         return self._size * self._size
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python3
+"""Module documentation - Define a class Square."""
 
 class Square:
-    def __init__(self, size=0):
-        """ Initialize the property size with the provided size."""
+    """Class documentation - Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
 
     @property
     def size(self):
-        """ Getter method for retrieving the size attribute."""
-        return self._size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """ Setter method for setting the size attribute and performing type and value validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self._size = value
+        self.__size = value
 
     def area(self):
-        """ Calculate and return the area of the square."""
-        return self._size * self._size
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """ Print the square using the '#' character."""
-        if self._size == 0:
-            print()
-        else:
-            for _ in range(self._size):
-                print("#" * self._size)
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
