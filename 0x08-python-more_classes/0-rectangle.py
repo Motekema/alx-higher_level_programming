b219b4a0285893b8a852a36983450330cd0e14e3
Motekema <motekema@gmail.com>
2023-10-02 09:53:08 +0000

    My commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# This is a simple Python class that defines a rectangle.
+
+class Rectangle:
+    """
+    This class represents a rectangle.
+
+    Attributes:
+        No attributes are defined for an empty rectangle class.
+    """
+
+    pass
