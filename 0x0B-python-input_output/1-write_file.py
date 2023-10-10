34dabdec04067fd9ed7e77c94a9fab525831cea5
Motekema <motekema@gmail.com>
2023-10-10 11:51:15 +0000

    My commit

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,14 +3,13 @@
 
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF-8 text file.
+    """Write a string to a UTF8 text file.
 
     Args:
         filename (str): The name of the file to write.
         text (str): The text to write to the file.
-
     Returns:
-        int: The number of characters written.
+        The number of characters written.
     """
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
