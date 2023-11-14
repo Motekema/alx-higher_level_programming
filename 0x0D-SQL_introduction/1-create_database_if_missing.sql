9d66c271cc3fec5a5d986dcf6fc4b6bcb791ea4b
Motekema <motekema@gmail.com>
2023-11-14 05:29:39 -0800

    My commit

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,9 @@
+-- Script to create the database hbtn_0c_0 if it does not exist
+
+-- Checking if the database exists
+-- If not exists, create the database
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- Switching to the hbtn_0c_0 database
+USE hbtn_0c_0;
+
