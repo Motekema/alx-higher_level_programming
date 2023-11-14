cc76c4f10fe9700cb133f811696b0226a9234f44
Motekema <motekema@gmail.com>
2023-11-14 05:32:03 -0800

    My commit

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- Script to delete the database hbtn_0c_0 if it exists
+
+-- Dropping the database if it exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
+
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- Script to list all tables of a specified database
+
+-- Displaying the tables in the specified database
+SHOW TABLES;
+
