0d26073851169520bfc7b40e00c675cf5da770b9
Motekema <motekema@gmail.com>
2023-11-15 10:12:56 -0800

    My commit

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,14 +1,6 @@
 -- 5-unique_id.sql
 
 -- Create table unique_id if it does not exist
-CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1 UNIQUE,
-    name VARCHAR(256)
-);
-
 -- Insert data into unique_id
-INSERT INTO unique_id (id, name) VALUES (89, 'Best School');
-
 -- Query data from unique_id
-SELECT * FROM unique_id;
-
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
