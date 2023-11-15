0ebfe715c9776053c99834bc04ec4fb0e1875e97
Motekema <motekema@gmail.com>
2023-11-15 09:59:24 -0800

    My commit

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,15 +1,5 @@
 -- 3-force_name.sql
 
 -- Create table force_name if it does not exist
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-
 -- Insert data into force_name
-INSERT INTO force_name (id, name) VALUES (89, 'Best School');
-
--- Query data from force_name
-SELECT * FROM force_name;
-
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
