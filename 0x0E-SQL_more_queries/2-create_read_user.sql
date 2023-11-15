fd5f270f000e3834152daf13511575824ccaee2a
Motekema <motekema@gmail.com>
2023-11-15 09:54:26 -0800

    My commit

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,17 @@
 -- 2-create_read_user.sql
 
 -- Create database hbtn_0d_2 if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create user user_0d_2 if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+-- Grant usage privilege on all databases to user_0d_2
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+
+-- Grant SELECT privilege on hbtn_0d_2 database to user_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
+
