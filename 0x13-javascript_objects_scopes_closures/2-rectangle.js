39af976363acf201dc8268f52d006c9b4789a9fc
Motekema <motekema@gmail.com>
2023-12-12 10:00:46 -0800

    My commit

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-/* Define a class Rectangle with constructor */
-
 class Rectangle {
   constructor(w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     } else {
-      // Create an empty object if width or height is not a positive integer
       return {};
     }
   }
