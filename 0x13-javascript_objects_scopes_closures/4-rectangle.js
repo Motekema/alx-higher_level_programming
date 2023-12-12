5fb9ce1d0c07161edbee71f5eb6d0041cef536ea
Motekema <motekema@gmail.com>
2023-12-12 10:08:24 -0800

    My commit

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,33 +1,29 @@
 #!/usr/bin/node
-/* Define a class Rectangle with constructor, print, rotate, and double methods */
-
 class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
-    } else {
-      // Create an empty object if width or height is not a positive integer
-      return {};
     }
   }
 
-  print() {
-    // Print the rectangle using the character 'X'
+  print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
     }
   }
 
-  rotate() {
-    // Exchange the width and height of the rectangle
-    const temp = this.width;
+  rotate () {
+    const aux = this.width;
     this.width = this.height;
-    this.height = temp;
+    this.height = aux;
   }
 
-  double() {
-    // Multiply the width and height of the rectangle by 2
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
