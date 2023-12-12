5dc8d92e6851364f38b6ce8b7b45d25fc83da4a3
Motekema <motekema@gmail.com>
2023-12-12 10:02:57 -0800

    My commit

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
-    } else {
-      return {};
     }
   }
 }
