30cd2a5530667f097d1c8f34cbb19c28ddf01f97
Motekema <motekema@gmail.com>
2023-12-12 10:10:37 -0800

    My commit

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-/* Define a class Square that inherits from Rectangle */
-
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  constructor(size) {
-    // Call the constructor of the base class (Rectangle) with size for both width and height
+  constructor (size) {
     super(size, size);
   }
 }
