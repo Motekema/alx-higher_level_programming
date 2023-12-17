f51f17d8997cabd5598db40f452d2e0b6d8efa4f
Motekema <motekema@gmail.com>
2023-12-17 11:18:10 -0800

    My commit

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,23 +7,14 @@ from sqlalchemy.orm import Session
 from model_state import Base, State
 from model_city import City
 
-if __name__ == "__main__":
-    # Connect to the MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+f __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
-    # Create a configured "Session" class
-    Session = Session(bind=engine)
-
-    # Create a Session
+    Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query all City objects and print the results
-    cities = session.query(City).order_by(City.id).all()
-    for city in cities:
-        state_name = session.query(State.name).filter(State.id == city.state_id).first()[0]
-        print("{}: ({}) {}".format(state_name, city.id, city.name))
-
-    # Close the session
-    session.close()
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
