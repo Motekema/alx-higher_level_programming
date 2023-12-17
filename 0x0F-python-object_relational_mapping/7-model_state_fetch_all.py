fe5e03cc2ffaa0a7df26e4b9dc7dabe8a4059567
Motekema <motekema@gmail.com>
2023-12-17 10:52:30 -0800

    My commit

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,9 +2,10 @@
 """Lists all State objects from the database hbtn_0e_6_usa"""
 
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmake
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
