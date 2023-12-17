84d30eaeb74d3efc8dd8fe561af61863b8754a0d
Motekema <motekema@gmail.com>
2023-12-17 10:42:02 -0800

    My commit

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,48 +6,12 @@ This script lists all cities of a given state from the database hbtn_0e_4_usa
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    # Check the number of command-line arguments
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Extract command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-        charset="utf8"
-    )
-
-    # Create a cursor object to interact with the database
-    cursor = db.cursor()
-
-    # Create and execute the SQL query
-    query = """
-    SELECT cities.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC
-    """
-    cursor.execute(query, (state_name,))
-
-    # Fetch all the rows
-    rows = cursor.fetchall()
-
-    # Display the results
-    city_names = [row[0] for row in rows]
-    print(', '.join(city_names))
-
-    # Close cursor and database connection
-    cursor.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
