bcb9d015af527b6b8c4580c873a484a5834fe8df
Motekema <motekema@gmail.com>
2024-01-05 02:51:23 -0800

    My commit

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script makes a request to 0.0.0.0:5000/catch_me causing the server to respond with "You got me!"
-
-curl -s -X PUT -L -d "user_id=98" "0.0.0.0:5000/catch_me"
+# This script makes a request to 0.0.0.0:5000/catch_me causing the server
+curl -sX PUT -L -d "user_id=98" --header "origin: HolbertonSchool" 0.0.0.0:5000/catch_me
