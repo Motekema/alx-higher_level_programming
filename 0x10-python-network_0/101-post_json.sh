21d3cc3546f1cc344062132d2d6df61b7ec41cac
Motekema <motekema@gmail.com>
2024-01-05 02:45:52 -0800

    My commit

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# This script sends a JSON POST request to a URL passed as the first argument,
-# and displays the body of the response. The JSON content is read from a file
-# passed as the second argument.
-
-file_content=$(cat "$2")
-
-if jq -e . >/dev/null 2>&1 <<<"$file_content"; then
-    curl -s -H "Content-Type: application/json" -X POST -d @"$2" "$1"
-else
-    echo "Not a valid JSON"
-fi
+# This script sends a JSON POST request to a URL passed as the first argument
+curl -sL -H "content-type:application/json"  -d @"$2" -X POST "$1"
