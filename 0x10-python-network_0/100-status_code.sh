70ae76fcdbebf118ce82d901e93a923055345812
Motekema <motekema@gmail.com>
2024-01-05 02:40:52 -0800

    My commit

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a request to a URL passed as an argument and displays only the status code of the response.
-
+# This script sends a request to a URL passed as an argument and displays.
 curl -s -o /dev/null -w "%{http_code}" "$1"
