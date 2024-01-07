745dc21a6801c59ac8ea35d4f3d99561e401c77a
Motekema <motekema@gmail.com>
2024-01-07 06:46:51 -0800

    My commit

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,27 +3,20 @@
 Script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
 
-import requests
 import sys
+import requests
+
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        q = ""
-    else:
-        q = sys.argv[1]
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
-    # Send a POST request to the URL with the letter as a parameter
-    response = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
-
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        # Parse the JSON response
-        json_response = response.json()
-
-        # Check if the JSON is not empty
-        if json_response:
-            print("[{}] {}".format(json_response['id'], json_response['name']))
-        else:
+        response = r.json()
+        if response == {}:
             print("No result")
-
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
