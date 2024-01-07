537f4a506639393e9416bd7fc9e802374196b542
Motekema <motekema@gmail.com>
2024-01-07 06:41:00 -0800

    My commit

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,22 +5,15 @@ Script that takes in a URL, sends a request to the URL, and displays the body of
 If the HTTP status code is greater than or equal to 400, it prints: Error code: followed by the value of the HTTP status code.
 """
 
-import requests
 import sys
+import requests
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <URL>".format(sys.argv[0]))
-        sys.exit(1)
-
     url = sys.argv[1]
 
-    # Send a GET request to the URL
-    response = requests.get(url)
-
-    # Display the body of the response
-    print(response.text)
-
-    # Check if the HTTP status code is greater than or equal to 400
-    if response.status_code >= 400:
-        print("Error code:", response.status_code)
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
