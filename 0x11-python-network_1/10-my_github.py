83c5984e69a10f02a779de0953f710cb6d2e1a76
Motekema <motekema@gmail.com>
2024-01-07 07:16:12 -0800

    My commit

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,29 +4,12 @@ Script that uses Basic Authentication with a personal access token
 to access the GitHub API and display the user ID.
 """
 
-import requests
 import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        sys.exit("Usage: {} <username> <personal_access_token>".format(sys.argv[0]))
-
-    username = sys.argv[1]
-    token = sys.argv[2]
-
-    # GitHub API endpoint for authenticated user details
-    url = "https://api.github.com/user"
-
-    # Set up Basic Authentication with username and personal access token
-    auth = (username, token)
-
-    # Send GET request to GitHub API with Basic Authentication
-    response = requests.get(url, auth=auth)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        user_info = response.json()
-        user_id = user_info.get('id')
-        print(user_id)
-    else:
-        print("None")
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
