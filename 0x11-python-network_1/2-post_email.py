26a30a491b2b620219801df629d0d134a4fb0a6c
Motekema <motekema@gmail.com>
2024-01-07 00:24:14 -0800

    My commit

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,18 +5,16 @@ to the passed URL with the email as a parameter, and displays
 the body of the response (decoded in utf-8).
 """
 
-import urllib.request
-import urllib.parse
 import sys
+import urllib.parse
+import urllib.request
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-
-    with urllib.request.urlopen(url, data=data) as response:
-        body = response.read().decode('utf-8')
-
-    print("Your email is: {}".format(email))
-    print(body)
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
