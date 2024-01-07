1f727b9cbe066ae0fa49854bd31aa18cb55fc508
Motekema <motekema@gmail.com>
2024-01-07 06:48:43 -0800

    My commit

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+Script that takes in a letter and sends a POST request to 
+http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
 
 import sys
