0d71a1c2ff9fead1c51de350092b53fd7300b345
Motekema <motekema@gmail.com>
2024-01-07 07:39:34 -0800

    My commit

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL, sends a request to the URL, and displays the body of the response.
-If the HTTP status code is greater than or equal to 400, it prints: Error code: followed by the value of the HTTP status code.
+Script that takes in a URL, sends a request to the URL,
+and displays the body of the response. If the HTTP status code
+is greater than or equal to 400, it prints an error message.
 """
 
 import sys
