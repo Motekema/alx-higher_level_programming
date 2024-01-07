6bd1bdd0fb23153041673c779764d2a447fb3d4c
Motekema <motekema@gmail.com>
2024-01-07 06:36:53 -0800

    My commit

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,22 +5,13 @@ sends a POST request to the passed URL with the email as a parameter,
 and finally displays the body of the response.
 """
 
-import requests
 import sys
+import requests
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: {} <URL> <email>".format(sys.argv[0]))
-        sys.exit(1)
-
     url = sys.argv[1]
-    email = sys.argv[2]
-    
-    # Data to be sent in the POST request
-    data = {'email': email}
-    
-    # Send POST request to the URL with email as a parameter
-    response = requests.post(url, data=data)
-    
-    # Display the body of the response
-    print("Your email is:", response.text)
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
