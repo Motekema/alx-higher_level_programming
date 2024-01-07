43854c5abce41fc58a5597a4c5183e29f4535962
Motekema <motekema@gmail.com>
2024-01-07 07:03:05 -0800

    My commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,30 +5,20 @@ of the repository “rails” by the user “rails” using GitHub API.
 """
 
 
-import requests
 import sys
+import requests
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        sys.exit("Usage: {} <repository name> <owner name>".format(sys.argv[0]))
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    repo_name = sys.argv[1]
-    owner_name = sys.argv[2]
-
-    # GitHub API endpoint for listing commits
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner_name, repo_name)
-
-    # Send GET request to GitHub API
-    response = requests.get(url)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        commits = response.json()[:10]  # Get the first 10 commits
-
-        # Print commits in the specified format
-        for commit in commits:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print("{}: {}".format(sha, author_name))
-    else:
-        print("Error: Unable to fetch commits. Status Code:", response.status_code)
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
