b77c0201ed44263ef10914f1f41cd191ad55d6c1
Motekema <motekema@gmail.com>
2024-01-30 04:17:15 -0800

    My commit

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const filmsData = JSON.parse(body);
+    const filmsWithWedgeAntilles = filmsData.results.filter((film) =>
+      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+    );
+
+    console.log(filmsWithWedgeAntilles.length);
+  }
+});
