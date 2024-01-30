55a4fe0c34796d889a4407ceda0746d3fcab315c
Motekema <motekema@gmail.com>
2024-01-30 04:19:21 -0800

    My commit

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
+      if (writeError) {
+        console.error(writeError);
+      } else {
+        console.log(`The content has been saved to ${filePath}`);
+      }
+    });
+  }
+});
