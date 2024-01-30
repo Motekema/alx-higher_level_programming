15dd4bc924ca50d0646f4e04f3e94a140d6cf0d7
Motekema <motekema@gmail.com>
2024-01-30 04:28:58 -0800

    My commit

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movieData = JSON.parse(body);
+    const charactersUrls = movieData.characters;
+
+    // Function to get character names from URLs
+    const getCharacterName = (characterUrl) => new Promise((resolve) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          const characterData = JSON.parse(body);
+          resolve(characterData.name);
+        }
+      });
+    });
+
+    // Fetch and print character names in the same order
+    Promise.all(charactersUrls.map(getCharacterName))
+      .then((characterNames) => {
+        characterNames.forEach((name) => console.log(name));
+      })
+      .catch((err) => console.error(err));
+  }
+});
