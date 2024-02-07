d87ee38be82f5e6190a01dcee9aa5bc63b472991
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-02-07 13:11:47 +0200

    My commit

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,5 +1,5 @@
-// Adds an LI element to the list when the div#add_item tag is clicked
-// The new element must be <li>Item</li> and must be added to UL.my_list
+// Added LI element to list when the div#add_item tag is clicked
+// New element must be <li>Item</li> and must be added to UL.my_list
 
 $('div#add_item').click(function () {
     let element = '<li>Item</li>';
