eabe082c49c9c56bfa15af934bcb8eb66a21f87b
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-02-07 13:12:43 +0200

    My commit

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,5 +1,5 @@
-// Updates the text of the header tag with a new header when the
-// DIV#update_header tag is clicked
+// Updated the text of the header tag with a new header
+// DIV#update_header tag clicked
 
 $('div#update_header').click(function () {
     $('header').text('New Header!!!');
