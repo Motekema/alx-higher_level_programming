b87251056443b20b043fc0d1e7538ed076e83d39
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-02-07 13:10:32 +0200

    My commit

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,5 +1,4 @@
-// Uses the jQuery API to add a red class to the header tag and turn it
-// red when the div#red_header tag is clicked
+// Uses the jQuery API to add a red class to the header tag.
 
 $('div#red_header').click(function () {
     $('header').addClass('red');
