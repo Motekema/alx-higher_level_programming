c8891fc45725f194adb8461747f5d98de00df2e3
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-02-07 13:09:57 +0200

    My commit

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,4 +1,4 @@
-// Uses jQuery API to change color of div#red_header to red on click
+// Uses jQuery API to change the color of div#red_header
 
 $('div#red_header').click(function () {
     $('header').css('color', '#FF0000');
