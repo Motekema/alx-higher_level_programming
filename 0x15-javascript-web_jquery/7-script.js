e363f8d6d9a9ab3056d5c0503569e21920ef766a
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-02-07 13:14:17 +0200

    My commit

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,5 +1,5 @@
-// Fetches and replaces the name of the API data then replaces the name
-// of the character in the DIV#character tag text
+// Fetched and replaced the name of the API data then replaced the name
+// Of the character in the DIV#character tag text
 
 let url = 'https://swapi.co/api/people/5/?format=json';
 $.get(url, function (data, stat) {
